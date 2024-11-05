0aa183278a67c455b7f9daefa6daee12d1cebe67
kiverix <137424284+Kiverix@users.noreply.github.com>
2024-11-05 16:02:45 +0100

    temperature

---
+++ temperature/temp.js
@@ -0,0 +1,20 @@
+let temp
+temp = 19
+
+if(temp<10){
+    console.log("Température faible")
+} else{
+    null
+}
+
+if(temp>10){
+    console.log("Température normale")
+} else{
+    null
+}
+
+if(temp>20){
+    console.log("Température Haute")
+} else{
+    null
+}
