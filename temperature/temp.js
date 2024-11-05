f0d232c73a457c1aa8422c2abcf5205e06fa84d4
kiverix <137424284+Kiverix@users.noreply.github.com>
2024-11-05 16:32:16 +0100

    tmp

--- temperature/temp.js
+++ temperature/temp.js
@@ -1,7 +1,21 @@
-let temp
-temp = 19
+let temp = document.getElementById("temp")
+let btn = document.getElementById("post")
+let temps = []
 
-if(temp<10){
+btn.addEventListener("click", handleClick)
+
+function handleClick(){
+    temp.value
+    if(temp<10){
+        alert("Temp Fbl")
+    } else if(temp>=10 && temp<20){
+        alert("Temp Nrml")
+    } else {
+        alert("Temp Hte")
+    }
+}
+
+/*if(temp<10){
     console.log("Température faible")
 } else{
     null
@@ -17,15 +31,15 @@ if(temp>20){
     console.log("Température Haute")
 } else{
     null
-}
+}*/
 
 //c'est cheesy mais j'ai la flemme de fair mieux
 //shit
 
-if(temp<10){
+/*if(temp<10){
     console.log("Temp Fbl")
 } else if(temp>=10 && temp<20){
     console.log("Temp Nrml")
 } else{
     console.log("Temp hte")
-}
+}*/
